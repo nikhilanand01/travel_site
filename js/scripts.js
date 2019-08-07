988b7074145950ec2f49e780eb47e08036b76cb8
Nikhil Anand <nikhil@fudinc.com>
2019-08-07 12:35:19 -0400

    milan modalssssss

--- js/scripts.js
+++ js/scripts.js
@@ -1442,110 +1442,29 @@ $("#btn9").on("click", function() {
   $(this).toggleClass("on");
 });
 
-// Text Guide Modal
-// Get the modal
-var modal = document.getElementById('myModal');
+// City Modal
+var modalBtns = [...document.querySelectorAll(".buttons")];
+modalBtns.forEach(function(btn){
+  btn.onclick = function() {
+    var modal = btn.getAttribute('data-modal');
+    document.getElementById(modal).style.display = "block";
+  }
+});
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("closemodal")[0];
-
-// When the user clicks the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
+var closeBtns = [...document.querySelectorAll(".close")];
+closeBtns.forEach(function(btn){
+  btn.onclick = function() {
+    var modal = btn.closest('.modal');
     modal.style.display = "none";
+  }
+});
+
+window.onclick = function(event) {
+  if (event.target.className === "modal") {
+    event.target.style.display = "none";
   }
 }
 
-// Tooltip
-// $( function()
-// {
-//     var targets = $( '[rel~=tooltip]' ),
-//         target  = false,
-//         tooltip = false,
-//         title   = false;
-//
-//     targets.bind( 'mouseenter', function()
-//     {
-//         target  = $( this );
-//         tip     = target.attr( 'title' );
-//         tooltip = $( '<div id="tooltip"></div>' );
-//
-//         if( !tip || tip == '' )
-//             return false;
-//
-//         target.removeAttr( 'title' );
-//         tooltip.css( 'opacity', 0 )
-//                .html( tip )
-//                .appendTo( 'body' );
-//
-//         var init_tooltip = function()
-//         {
-//             if( $( window ).width() < tooltip.outerWidth() * 1.5 )
-//                 tooltip.css( 'max-width', $( window ).width() / 2 );
-//             else
-//                 tooltip.css( 'max-width', 340 );
-//
-//             var pos_left = target.offset().left + ( target.outerWidth() / 2 ) - ( tooltip.outerWidth() / 2 ),
-//                 pos_top  = target.offset().top - tooltip.outerHeight() - 20;
-//
-//             if( pos_left < 0 )
-//             {
-//                 pos_left = target.offset().left + target.outerWidth() / 2 - 20;
-//                 tooltip.addClass( 'left' );
-//             }
-//             else
-//                 tooltip.removeClass( 'left' );
-//
-//             if( pos_left + tooltip.outerWidth() > $( window ).width() )
-//             {
-//                 pos_left = target.offset().left - tooltip.outerWidth() + target.outerWidth() / 2 + 20;
-//                 tooltip.addClass( 'right' );
-//             }
-//             else
-//                 tooltip.removeClass( 'right' );
-//
-//             if( pos_top < 0 )
-//             {
-//                 var pos_top  = target.offset().top + target.outerHeight();
-//                 tooltip.addClass( 'top' );
-//             }
-//             else
-//                 tooltip.removeClass( 'top' );
-//
-//             tooltip.css( { left: pos_left, top: pos_top } )
-//                    .animate( { top: '+=10', opacity: 1 }, 50 );
-//         };
-//
-//         init_tooltip();
-//         $( window ).resize( init_tooltip );
-//
-//         var remove_tooltip = function()
-//         {
-//             tooltip.animate( { top: '-=10', opacity: 0 }, 50, function()
-//             {
-//                 $( this ).remove();
-//             });
-//
-//             target.attr( 'title', tip );
-//         };
-//
-//         target.bind( 'mouseleave', remove_tooltip );
-//         tooltip.bind( 'click', remove_tooltip );
-//     });
-// });
 
 
 /*\
